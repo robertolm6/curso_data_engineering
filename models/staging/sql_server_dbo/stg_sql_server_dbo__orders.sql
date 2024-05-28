56daab9bfb3df1f83e22b6cf459f54abbee20be8
Roberto López Merino <robertolopez612@gmail.com>
2024-05-28 17:38:49 +0000

    Algunas transformaciones realizadas en la tabla promos y orders. Entre ellas, se han sacado algunas tablas nuevas a partir de ellas (status en el caso de promos y shipping service en el caso de orders).

--- models/staging/sql_server_dbo/stg_sql_server_dbo__orders.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__orders.sql
@@ -10,15 +10,21 @@ renamed_orders as (
 
     select
         order_id,
-        shipping_service,
-        shipping_cost,
+        case
+            when shipping_service = '' then md5('unknown')
+            else md5(shipping_service)
+        end as shipping_service_id,
+        shipping_cost as shipping_cost_dollars,
         address_id,
         created_at,
-        promo_id,
+        case
+            when promo_id = '' then md5('no promo')
+            else md5(promo_id)
+        end as promo_id,
         estimated_delivery_at,
-        order_cost,
+        order_cost as order_cost_dollars,
         user_id,
-        order_total,
+        order_total as order_total_dollars,
         delivered_at,
         tracking_id,
         status,
@@ -30,3 +36,8 @@ renamed_orders as (
 )
 
 select * from renamed_orders
+
+/*Cuando el shipping service es unknown, no hay fecha de entrega estimada, ni fecha de entrega, ni tracking id y el estado es preparing
+  Cuando el estado es shipped, no hay fecha de entrega pero sí fecha de entrega estimada.
+  Cuando el estado es delivered, todas las columnas están completas
+*/
